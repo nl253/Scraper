594260affba3f2f308dedf8faa8120280eeb6943
nl253 <nl253@kent.ac.uk>
2017-05-31 16:46:16 +0100

    Update.

--- theme_scraper.py
+++ theme_scraper.py
@@ -6,20 +6,19 @@ import db
 
 database = db.SQLite()
 
-# database.query("CREATE TABLE scraping ( url TEXT, chunk TEXT )")
-
 spider = Spider(
-    starting_urls=["https://en.wikipedia.org/wiki/Web_scraping",
-                   "http://python-guide-pt-br.readthedocs.io/en/latest/scenarios/scrape/"],
-    themes=["scraping",
-            "extraction",
-            "extract data",
-            "spider",
-            "data mining"],
-    max_entries=1500,
+    starting_urls=["https://en.wikipedia.org/wiki/Donald_Trump"],
+    themes=["election",
+            "Trump",
+            "D. Trump",
+            "Donald Trump",
+            "sexual harrasment",
+            "rape",
+            "accused"],
+    max_entries=2500,
     match_threshold=6)
 
 spider.scrape()
 
 for row in spider.ientries:
-    database.query("INSERT INTO scraping VALUES (?, ?)", data=row)
+    database.query("INSERT INTO trump VALUES (?, ?)", data=row)
