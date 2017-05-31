2a6806cb53519c88e8e4aa2ee595bda92d803cc2
nl253 <nl253@kent.ac.uk>
2017-05-31 20:15:37 +0100

    Update.

--- spiders.py
+++ spiders.py
@@ -11,7 +11,9 @@ from http_tools import HTMLExtractor
 from lexical import HTMLAnalyser, DocumentAnalayzer
 from socket import timeout
 from ssl import CertificateError
-# import multiprocessing
+from multiprocessing import cpu_count, Process
+# from asyncio import Semaphore
+from time import sleep
 
 logging.basicConfig(
     level=logging.INFO,
@@ -23,35 +25,90 @@ l = logging.getLogger(name=__name__)
 Entry = Tuple[Union[str, float, float, str], ...]
 
 class Spider():
-    def __init__(self, themes: List[str], max_entries=2000, match_threshold=18):
+    def __init__(self, starting_urls: List[str], themes: List[str],
+                 max_entries=2000, match_threshold=18, max_threads=cpu_count(),
+                 process_results_with=None, function_args=[]):
         self._max_entries = max_entries
+        self._max_threads = max_threads
         self._themes = themes
+        # min matches on a page to add entries and links
         self._match_threshold = match_threshold
+        # stack
         self._entries = LifoQueue()
         self._to_be_scraped = Counter()
+        self._to_be_scraped.update(starting_urls)
         self._processed_urls = set()
+        self._function = process_results_with
+        # self._counting_semaphore = Semaphore(value=self._max_threads)
+        self._counting_semaphore = self._max_threads
+        self._jobs = []
+        self._function_args = function_args
+        self._finished = False
+        assert type(function_args) is list, 'Extra args need to be passed as list.'
         l.info('Spider created')
         l.info("Max set to {}".format(max_entries))
         l.info("Themes set to {}".format(themes))
 
     def _add_entry(self, *entry: Entry):
-        self._entries.put(tuple(entry))
+        if self._function:
+            if self._function_args:
+                # if you supply your own funct to process the data, you need to
+                # make sure it outputs a tuple
+                entry = self._processing_func(*entry, *self._function_args)
+            else:
+                entry = self._processing_func(*entry)
+        else:
+            entry = tuple(entry)
+        assert type(entry) is tuple, 'Type of entry is not tuple.'
         l.info('Adding an entry')
+        self._entries.put(entry)
         l.info('No Entries: {}'.format(self._entries.qsize()))
 
     @property
     def ientries(self) -> Entry:
+        # you cannot call it before calling scrape()
+        while not self.finished:
+            sleep(10)
         while not self._entries.empty():
             yield self._entries.get()
 
-    def scrape(self, starting_urls: List[str]):
+    def scrape(self):
+        while len(self._to_be_scraped) > 0 and self._entries.qsize() < self._max_entries:
 
-        self._to_be_scraped.update(starting_urls)
+            # import pudb; pudb.set_trace()  # XXX BREAKPOINT
 
-        l.info("Starting URLs set to {}".format(starting_urls))
+            # if self._counting_semaphore.acquire():
+                # job = Process(target=self._scrape)
+                # job.start()
+                # import pudb; pudb.set_trace()  # XXX BREAKPOINT
+            # else:
+                # sleep(10)
+            if self._counting_semaphore > 0 and self._counting_semaphore < 5:
+                l.info('Starting another job')
+                self._jobs.append(Process(target=self._scrape))
+                self._counting_semaphore -= 1
+                self._jobs[len(self._jobs) - 1].start()
+            else:
+                l.info('Going to sleep')
+                sleep(10)
+                for job in self._jobs:
+                    if job is None or not job.is_alive():
+                        self._counting_semaphore += 1
+                        self._jobs.remove(job)
+                        l.info('Removing a dead job')
+
+        # set finished when esceped from the loop
+        l.info('Setting finished to True')
+        self._finished = True
+
+    @property
+    def finished(self) -> bool:
+        return self._finished
+
+    def _scrape(self):
 
         # loop until the queue is empty
-        while len(self._to_be_scraped) > 0 and self._entries.qsize() < self._max_entries:
+        while self._to_be_scraped is not None and len(self._to_be_scraped) > 0 and self._entries.qsize() < self._max_entries:
 
             l.info('{} URLs to scrape'.format(len(self._to_be_scraped)))
             l.info('{} already processed'.format(len(self._processed_urls)))
@@ -97,10 +154,11 @@ class Spider():
 
                 # populate the queue
                 l.info('Adding links from {} to to_be_scraped'.format(focus_url))
-                self._to_be_scraped = Counter(links) | self._to_be_scraped
+                self._to_be_scraped = self._to_be_scraped.update(links)
 
                 # l.info('To be scraped at the end of loop: {}'.format(self._to_be_scraped))
                 # l.info('Len of to be scraped at the end of loop: {}'.format(len(self._to_be_scraped)))
 
             else:
                 l.info('Not enough matches in {}, continuing'.format(focus_url))
+
--- theme_scraper.py
+++ theme_scraper.py
@@ -3,9 +3,7 @@
 
 from spiders import Spider
 import db
-import multiprocessing
 from time import sleep
-
 
 database = db.SQLite()
 
@@ -21,15 +19,14 @@ starting_urls = ["https://en.wikipedia.org/wiki/Alan_Turing",
                  "http://www.bbc.co.uk/timelines/z8bgr82",
                  "http://www.turing.org.uk/publications/dnb.html"]
 
-spider = Spider(max_entries=100, match_threshold=6, themes=themes)
+spider = Spider(starting_urls=starting_urls,
+                themes=themes, max_entries=100,
+                match_threshold=6)
 
-jobs = map(lambda url: multiprocessing.Process(target=spider.scrape, args=([url],)), starting_urls)
+spider.scrape()
 
-for job in jobs:
-    job.start()
+for i in spider.ientries:
+    print(i)
 
-while not all(map(lambda job: job.read(), jobs)):
-    sleep(30)
-
-for row in spider.ientries:
-    database.query("INSERT INTO turing VALUES (?, ?)", data=row)
+# for row in spider.ientries:
+    # database.query("INSERT INTO turing VALUES (?, ?)", data=row)
