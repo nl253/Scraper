c844c5925c6d7839550182397b8744e5d1ce88f4
nl253 <nl253@kent.ac.uk>
2017-05-31 17:28:01 +0100

    Update with parallelism.

--- spiders.py
+++ spiders.py
@@ -11,6 +11,7 @@ from http_tools import HTMLExtractor
 from lexical import HTMLAnalyser, DocumentAnalayzer
 from socket import timeout
 from ssl import CertificateError
+# import multiprocessing
 
 logging.basicConfig(
     level=logging.INFO,
@@ -22,19 +23,16 @@ l = logging.getLogger(name=__name__)
 Entry = Tuple[Union[str, float, float, str], ...]
 
 class Spider():
-    def __init__(self, starting_urls: List[str], themes: List[str], max_entries=2000, match_threshold=18):
+    def __init__(self, themes: List[str], max_entries=2000, match_threshold=18):
         self._max_entries = max_entries
         self._themes = themes
-        self._focus_url = ""
         self._match_threshold = match_threshold
         self._entries = LifoQueue()
         self._to_be_scraped = Counter()
-        self._to_be_scraped.update(starting_urls)
         self._processed_urls = set()
         l.info('Spider created')
         l.info("Max set to {}".format(max_entries))
         l.info("Themes set to {}".format(themes))
-        l.info("Starting URLs set to {}".format(starting_urls))
 
     def _add_entry(self, *entry: Entry):
         self._entries.put(tuple(entry))
@@ -46,9 +44,11 @@ class Spider():
         while not self._entries.empty():
             yield self._entries.get()
 
-    def scrape(self):
+    def scrape(self, starting_urls: List[str]):
 
-        # import pudb; pudb.set_trace()  # XXX BREAKPOINT
+        self._to_be_scraped.update(starting_urls)
+
+        l.info("Starting URLs set to {}".format(starting_urls))
 
         # loop until the queue is empty
         while len(self._to_be_scraped) > 0 and self._entries.qsize() < self._max_entries:
@@ -57,37 +57,37 @@ class Spider():
             l.info('{} already processed'.format(len(self._processed_urls)))
 
             # get next from from queue
-            self._focus_url = self._to_be_scraped.most_common(1)[0][0]
+            focus_url = self._to_be_scraped.most_common(1)[0][0]
 
-            self._to_be_scraped.pop(self._focus_url)
+            self._to_be_scraped.pop(focus_url)
 
-            l.info('Focus URL: {}'.format(self._focus_url))
+            l.info('Focus URL: {}'.format(focus_url))
 
             # add to traversed to prevent visitng twice
-            self._processed_urls.add(self._focus_url)
+            self._processed_urls.add(focus_url)
 
             try:
                 # instantiate an extractor object
-                extractor = HTMLExtractor(self._focus_url)
+                extractor = HTMLExtractor(focus_url)
                 # get no matches in html
                 matches = HTMLAnalyser(extractor.HTML, self._themes).theme_count
 
             except (IncompleteRead,HTTPError,URLError,RemoteDisconnected,UnicodeDecodeError,UnicodeEncodeError,timeout,CertificateError):
-                l.debug('Error while requesting a response for {}'.format(self._focus_url))
+                l.debug('Error while requesting a response for {}'.format(focus_url))
                 l.debug('Continuing')
                 continue
 
 
             # count matches on the focus page
-            l.info('Found {} matches in the content of {}'.format(matches, self._focus_url))
+            l.info('Found {} matches in the content of {}'.format(matches, focus_url))
 
             if matches >= self._match_threshold:
 
                 l.info('Enough matches, adding results and extracting links')
 
-                l.info('Adding results from {} to entries'.format(self._focus_url))
+                l.info('Adding results from {} to entries'.format(focus_url))
                 for sent in DocumentAnalayzer(extractor.text, themes=self._themes).matching_sents:
-                    self._add_entry(self._focus_url, sent)
+                    self._add_entry(focus_url, sent)
 
                 # ensure you only traverse once
                 l.info('Filetering extracted links')
@@ -96,11 +96,11 @@ class Spider():
                 links = filter(lambda link: link not in self._processed_urls, extractor.URLs)
 
                 # populate the queue
-                l.info('Adding links from {} to to_be_scraped'.format(self._focus_url))
+                l.info('Adding links from {} to to_be_scraped'.format(focus_url))
                 self._to_be_scraped = Counter(links) | self._to_be_scraped
 
                 # l.info('To be scraped at the end of loop: {}'.format(self._to_be_scraped))
                 # l.info('Len of to be scraped at the end of loop: {}'.format(len(self._to_be_scraped)))
 
             else:
-                l.info('Not enough matches in {}, continuing'.format(self._focus_url))
+                l.info('Not enough matches in {}, continuing'.format(focus_url))
--- theme_scraper.py
+++ theme_scraper.py
@@ -3,22 +3,33 @@
 
 from spiders import Spider
 import db
+import multiprocessing
+from time import sleep
+
 
 database = db.SQLite()
 
-spider = Spider(
-    starting_urls=["https://en.wikipedia.org/wiki/Donald_Trump"],
-    themes=["election",
-            "Trump",
-            "D. Trump",
-            "Donald Trump",
-            "sexual harrasment",
-            "rape",
-            "accused"],
-    max_entries=2500,
-    match_threshold=6)
+themes=["A. Turing",
+        "computer science",
+        "Turing",
+        "k",
+        "A Turing",
+        "Turing Machine",
+        "finate automata"]
 
-spider.scrape()
+starting_urls = ["https://en.wikipedia.org/wiki/Alan_Turing",
+                 "http://www.bbc.co.uk/timelines/z8bgr82",
+                 "http://www.turing.org.uk/publications/dnb.html"]
+
+spider = Spider(max_entries=100, match_threshold=6, themes=themes)
+
+jobs = map(lambda url: multiprocessing.Process(target=spider.scrape, args=([url],)), starting_urls)
+
+for job in jobs:
+    job.start()
+
+while not all(map(lambda job: job.read(), jobs)):
+    sleep(30)
 
 for row in spider.ientries:
-    database.query("INSERT INTO trump VALUES (?, ?)", data=row)
+    database.query("INSERT INTO turing VALUES (?, ?)", data=row)
