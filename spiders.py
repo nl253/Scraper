3ad12f43dce1f4e232849ee076dc97a21347461f
nl253 <nl253@kent.ac.uk>
2017-05-31 23:02:33 +0100

    Done.

--- spiders.py
+++ spiders.py
@@ -5,14 +5,13 @@ from typing import Tuple, Union, List
 from urllib.error import HTTPError, URLError
 from http.client import RemoteDisconnected, IncompleteRead
 import logging
-from queue import LifoQueue
-from collections import Counter
 from http_tools import HTMLExtractor
 from lexical import HTMLAnalyser, DocumentAnalayzer
 from socket import timeout
 from ssl import CertificateError
 from multiprocessing import cpu_count, Process
-# from asyncio import Semaphore
+from multiprocessing import Queue as SharedQueue
+from multiprocessing import Semaphore, current_process
 from time import sleep
 
 logging.basicConfig(
@@ -28,28 +27,33 @@ class Spider():
     def __init__(self, starting_urls: List[str], themes: List[str],
                  max_entries=2000, match_threshold=18, max_threads=cpu_count(),
                  process_results_with=None, function_args=[]):
+        # settings
         self._max_entries = max_entries
         self._max_threads = max_threads
         self._themes = themes
         # min matches on a page to add entries and links
         self._match_threshold = match_threshold
-        # stack
-        self._entries = LifoQueue()
-        self._to_be_scraped = Counter()
-        self._to_be_scraped.update(starting_urls)
-        self._processed_urls = set()
-        self._function = process_results_with
-        # self._counting_semaphore = Semaphore(value=self._max_threads)
-        self._counting_semaphore = self._max_threads
+        # stack to store scraped data as tuples
+        self._entries = SharedQueue()
+        self._to_be_scraped = SharedQueue(20000)
         self._jobs = []
+        for url in starting_urls:
+            self._to_be_scraped.put(url)
+        # efficient lookup
+        self._processed_urls = set()
+        self._counting_semaphore = Semaphore(self._max_threads)
+        self._finished = False  # initialise to False
+        self._function = process_results_with
         self._function_args = function_args
-        self._finished = False
         assert type(function_args) is list, 'Extra args need to be passed as list.'
         l.info('Spider created')
-        l.info("Max set to {}".format(max_entries))
+        l.info("Max entries set to {}".format(max_entries))
         l.info("Themes set to {}".format(themes))
+        l.info("Max threads set to {}".format(max_threads))
+        l.info("Match threshold set to {}".format(match_threshold))
 
     def _add_entry(self, *entry: Entry):
+        curr_proc = current_process().name
         if self._function:
             if self._function_args:
                 # if you supply your own funct to process the data, you need to
@@ -60,9 +64,9 @@ class Spider():
         else:
             entry = tuple(entry)
         assert type(entry) is tuple, 'Type of entry is not tuple.'
-        l.info('Adding an entry')
+        l.info('{}: Adding an entry'.format(curr_proc))
         self._entries.put(entry)
-        l.info('No Entries: {}'.format(self._entries.qsize()))
+        l.info('{}: No Entries: {}'.format(curr_proc, self._entries.qsize()))
 
     @property
     def ientries(self) -> Entry:
@@ -73,32 +77,29 @@ class Spider():
             yield self._entries.get()
 
     def scrape(self):
-        while len(self._to_be_scraped) > 0 and self._entries.qsize() < self._max_entries:
 
-            # import pudb; pudb.set_trace()  # XXX BREAKPOINT
+        while self._entries.qsize() < self._max_entries and self._to_be_scraped.qsize() > 0:
 
-            # if self._counting_semaphore.acquire():
-                # job = Process(target=self._scrape)
-                # job.start()
-                # import pudb; pudb.set_trace()  # XXX BREAKPOINT
-            # else:
-                # sleep(10)
-            if self._counting_semaphore > 0 and self._counting_semaphore < 5:
+            if self._counting_semaphore.acquire():
                 l.info('Starting another job')
-                self._jobs.append(Process(target=self._scrape))
-                self._counting_semaphore -= 1
+                job = Process(target=self._scrape)
+                self._jobs.append(job)
                 self._jobs[len(self._jobs) - 1].start()
+
             else:
                 l.info('Going to sleep')
                 sleep(10)
-                for job in self._jobs:
-                    if job is None or not job.is_alive():
-                        self._counting_semaphore += 1
-                        self._jobs.remove(job)
-                        l.info('Removing a dead job')
+
+        # once we have enough entries
+        l.info('Setting finished to True')
+        while not all(map(lambda job: not job.is_alive(), self._jobs)):
+            for job in self._jobs:
+                if job.is_alive():
+                    job.terminate()
+                    self._jobs.remove(job)
+            sleep(10)
 
         # set finished when esceped from the loop
-        l.info('Setting finished to True')
         self._finished = True
 
     @property
@@ -106,19 +107,25 @@ class Spider():
         return self._finished
 
     def _scrape(self):
+        """To be run by each process thread.
+        """
+        curr_proc = current_process().name
 
         # loop until the queue is empty
-        while self._to_be_scraped is not None and len(self._to_be_scraped) > 0 and self._entries.qsize() < self._max_entries:
+        # assert type(self._to_be_scraped) is , 'Type of self._to_be_scraped is not Counter!'
+        while self._entries.qsize() < self._max_entries and self._to_be_scraped.qsize() > 0:
 
-            l.info('{} URLs to scrape'.format(len(self._to_be_scraped)))
-            l.info('{} already processed'.format(len(self._processed_urls)))
+            l.info('{}: {} URLs to scrape'.format(curr_proc, self._to_be_scraped.qsize()))
+            l.info('{}: {} already processed'.format(curr_proc, len(self._processed_urls)))
 
             # get next from from queue
-            focus_url = self._to_be_scraped.most_common(1)[0][0]
+            focus_url = self._to_be_scraped.get()
 
-            self._to_be_scraped.pop(focus_url)
+            if focus_url in self._processed_urls:
+                l.info('{}: current URL {} has already been processed, continuing'.format(curr_proc, focus_url))
+                continue
 
-            l.info('Focus URL: {}'.format(focus_url))
+            l.info('{}: Focus URL: {}'.format(curr_proc, focus_url))
 
             # add to traversed to prevent visitng twice
             self._processed_urls.add(focus_url)
@@ -130,35 +137,38 @@ class Spider():
                 matches = HTMLAnalyser(extractor.HTML, self._themes).theme_count
 
             except (IncompleteRead,HTTPError,URLError,RemoteDisconnected,UnicodeDecodeError,UnicodeEncodeError,timeout,CertificateError):
-                l.debug('Error while requesting a response for {}'.format(focus_url))
-                l.debug('Continuing')
+                l.debug('{}: Error while requesting a response for {}'.format(curr_proc, focus_url))
+                l.debug('{}: Continuing'.format(curr_proc))
                 continue
 
 
             # count matches on the focus page
-            l.info('Found {} matches in the content of {}'.format(matches, focus_url))
+            l.info('{}: Found {} matches in the content of {}'.format(curr_proc, matches, focus_url))
 
             if matches >= self._match_threshold:
 
                 l.info('Enough matches, adding results and extracting links')
 
-                l.info('Adding results from {} to entries'.format(focus_url))
+                l.info('{}: Adding results from {} to entries'.format(curr_proc, focus_url))
                 for sent in DocumentAnalayzer(extractor.text, themes=self._themes).matching_sents:
                     self._add_entry(focus_url, sent)
 
                 # ensure you only traverse once
-                l.info('Filetering extracted links')
+                l.info('{}: Filetering extracted links'.format(curr_proc))
 
                 # check for titles if they match any of the themes
                 links = filter(lambda link: link not in self._processed_urls, extractor.URLs)
 
                 # populate the queue
-                l.info('Adding links from {} to to_be_scraped'.format(focus_url))
-                self._to_be_scraped = self._to_be_scraped.update(links)
+                # l.info('{}: Adding links from {} to to_be_scraped'.format(curr_proc, focus_url))
+                # self._to_be_scraped = self._to_be_scraped.update(links)
+                for link in links:
+                    self._to_be_scraped.put(link)
 
                 # l.info('To be scraped at the end of loop: {}'.format(self._to_be_scraped))
                 # l.info('Len of to be scraped at the end of loop: {}'.format(len(self._to_be_scraped)))
 
             else:
-                l.info('Not enough matches in {}, continuing'.format(focus_url))
+                l.info('{}: Not enough matches in {}, continuing'.format(curr_proc, focus_url))
 
+        self._counting_semaphore.release()
